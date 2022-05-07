2d6005f3b70352f19eb753aec072c80514c04280
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-05-07 15:05:44 +0600

    login validation added

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -14,9 +14,38 @@ import {
     Image,
 } from '@chakra-ui/react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import axios from 'axios';
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
+    const [data, setData] = useState({ email: "", password: "" });
+    const [error, setError] = useState("");
+
+
+    const handleChange = ({ currentTarget: input }) => {
+        setData({ ...data, [input.name]: input.value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const url = '';
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (data.email.match(mailformat) && data.email !== "" && data.password !== "") {
+            axios.post(url, data)
+                .then((res) => {
+                    localStorage.setItem('token', res.data);
+                    setError('');
+                })
+                .catch((err) => {
+                    setError(err);
+                })
+        }
+        if (!data.email.match(mailformat)) {
+            setError(`You have entered an invalid email address!`)
+        }
+        if (data.password === "") {
+            setError(`"Password" is not allowed to be empty`)
+        }
+    };
     return (
         <Flex
             minH={'100vh'}
@@ -38,19 +67,34 @@ const Login = () => {
                     <Stack spacing={4}>
                         <FormControl id="email">
                             <FormLabel>Email</FormLabel>
-                            <Input placeholder='john@due.com' type="email" />
+                            <Input
+                                name="email"
+                                onChange={handleChange}
+                                value={data.email}
+                                required
+                                placeholder='john@due.com'
+                                type="email" />
                         </FormControl>
                         <FormControl id="password">
                             <FormLabel>Password</FormLabel>
-                            <Input placeholder='********' type={showPassword ? "text" : "password"} />
+                            <Input
+                                placeholder='********'
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                onChange={handleChange}
+                                value={data.password}
+                                required
+                            />
                             <InputRightElement width="4.5rem">
                                 <Button bgColor='transparent' marginLeft="20px" marginBottom="-105px" h="1.75rem" size="sm" onClick={() => setShowPassword(!showPassword)}>
                                     {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                                 </Button>
                             </InputRightElement>
                         </FormControl>
+                        {error && <Box color="red" className="error_msg">{error}</Box>}
                         <Stack spacing={10}>
                             <Button
+                                onClick={handleSubmit}
                                 marginTop='20px'
                                 bg={'#f22224'}
                                 color={'white'}
